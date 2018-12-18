ff31fa3ecd8777ea186b7306516f2c9a276355ef
Taha Azzaoui <taha@tahaazzaoui.com>
2018-12-17 23:23:45 -0500

    Added credentials file

---
+++ credentials.py
@@ -0,0 +1,9 @@
+#Twitter API credentials
+consumer_key = "YOUR CONSUMER KEY"
+consumer_secret = "YOUR CONSUMER SECRET"
+access_key = "YOUR ACCESS KEY"
+access_secret = "YOUR ACCESS SECRET"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
